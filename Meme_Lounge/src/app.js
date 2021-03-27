a82e4188da5bbbdae05bda25f247c8a3cde20ec2
Valeri Gochev <csvalkata@gmail.com>
2021-03-27 20:47:30 +0200

    fixed some resources problems

--- Meme_Lounge/src/app.js
+++ Meme_Lounge/src/app.js
@@ -1,5 +1,5 @@
-import page from '../node_modules/page/page.mjs';
-import { render } from '../node_modules/lit-html/lit-html.js';
+import page from '/node_modules/page/page.mjs';
+import { render } from '/node_modules/lit-html/lit-html.js';
 
 import {logout} from './api/data.js';
 
