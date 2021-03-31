38bec0e5a9692365125a65d2d1dd050116541d7a
Valeri <csvalkata@gmail.com>
2021-03-31 12:30:32 +0300

    API.js added

---
+++ requestAPI/api.js
@@ -0,0 +1,90 @@
+// set your rest service host
+export const settings = {
+    host: 'http://localhost:3030'
+}
+
+// Main request function
+async function request(url, options = {}) {
+    try {
+        const response = await fetch(url, options);
+
+        if(response.ok == false) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        try {
+            const data = await response.json();
+            return data;
+        } catch (err) {
+            return response;
+        }
+
+    } catch (err) {
+        console.error(err.message);
+        alert(error.message);
+        throw err;
+    }
+}
+
+
+// Create Options function
+function createOptions(method = 'GET', data) {
+    let options = {
+        method,
+        headers: {}
+    }
+
+    if(data){
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const token = sessionStorage.getItem('authToken');
+    if(token != null) {
+        options.headers['X-Authorization'] = token;
+    }
+    
+    return options;
+}
+
+
+// CRUD Functions
+export async function get(url) {
+    return await request(url, createOptions());
+}
+
+export async function post(url, data) {
+    return await request(url, createOptions('POST', data));
+}
+
+export async function put(url, data) {
+    return await request(url, createOptions('PUT', data));
+}
+
+export async function del(url) {
+    return await request(url, createOptions('DELETE'));
+}
+
+// AUTHENTICATION
+
+export async function login(email, password) {
+    const response = await post(settings.host + '/users/login', { email, password });
+    sessionStorage.setItem('authToken', response.accessToken);
+    sessionStorage.setItem('userId', response._id);
+    sessionStorage.setItem('email', response.email);
+    return response;
+}
+
+export async function register(email, password) {
+    const response = await post(settings.host + '/users/register', { email, password });
+    sessionStorage.setItem('authToken', response.accessToken);
+    sessionStorage.setItem('userId', response._id);
+    sessionStorage.setItem('email', response.email);
+    return response;
+}
+
+export async function logout() {
+    await get(settings.host + '/users/logout');
+    sessionStorage.clear(); //or remove them one by one
+}
